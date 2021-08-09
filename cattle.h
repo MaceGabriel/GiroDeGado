fd505b3ea0507c37d69b1878d81e2cc1949cb774
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2021-08-09 20:18:34 -0300

    Classe Cattle

---
+++ cattle.h
@@ -0,0 +1,119 @@
+// #ifndef CATTLE_H
+// #define CATTLE_H
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// //! Class Cattle
+// /**
+// * This Class represents a cattle for the GiroDeGado software implemented in this code.
+// */
+// class Cattle{
+
+//     public:
+//         /*!
+//             This is the default destructor for the Cattle Class.
+//         */
+//         virtual ~Cattle(){}
+     
+//         /*!
+//             Sets the earring attribute in the Cattle Class.
+//             \param cattle_earring which will be set to the current Cattle.
+//         */
+//         virtual void setEarring(string cattle_earring) = 0;
+
+//         /*!
+//             Returns the earring attribute in the Cattle Class.
+//             \return string - the content earring attribute.  
+//         */
+//         virtual string getEarring() const = 0;
+        
+//         /*!
+//             Sets the breed attribute in the Cattle Class.
+//             \param cattle_breed which will be set to the current Cattle.
+//         */
+//         virtual void setBreed(string cattle_breed) = 0;
+
+//         /*!
+//             Returns the breed attribute in the Cattle Class.
+//             \return string - the content Breed attribute.  
+//         */
+//         virtual string getBreed() const = 0;
+
+//         /*!
+//             Sets the acquisition date attribute in the Cattle Class.
+//             \param cattle_acquisition_date which will be set to the current Cattle.
+//         */
+//         virtual void setAcquisitionDate(string cattle_acquisition_date) = 0;
+
+//         /*!
+//             Returns the acquisition date attribute in the Cattle Class.
+//             \return string - the content acquisition date attribute.  
+//         */
+//         virtual string getAcquisitionDate() const = 0;
+
+//         /*!
+//             Sets the birth date attribute in the Cattle Class.
+//             \param cattle_birth_date which will be set to the current Cattle.
+//         */
+//         virtual void setBirthDate(string cattle_birth_date) = 0;
+
+//         /*!
+//             Returns the birth date attribute in the Cattle Class.
+//             \return string - the content birth date attribute.  
+//         */
+//         virtual string getBirthDate() const = 0;
+
+//         /*!
+//             Sets the father attribute in the Cattle Class.
+//             \param cattle_father which will be set to the current Cattle.
+//         */
+//         virtual void setFather(string cattle_father) = 0;
+
+//         /*!
+//             Returns the father attribute in the Cattle Class.
+//             \return string - the content father attribute.  
+//         */
+//         virtual string getFather() const = 0;
+
+//         /*!
+//             Sets the mother attribute in the Cattle Class.
+//             \param cattle_mother which will be set to the current Cattle.
+//         */
+//         virtual void setMother(string cattle_mother) = 0;
+
+//         /*!
+//             Returns the mother attribute in the Cattle Class.
+//             \return string - the content mother attribute.  
+//         */
+//         virtual string getMother() const = 0;
+
+//         /*!
+//             Sets the weight attribute in the Cattle Class.
+//             \param cattle_weight which will be set to the current Cattle.
+//         */
+//         virtual void setWeight(double cattle_weight) = 0;
+
+//         /*!
+//             Returns the weight attribute in the Cattle Class.
+//             \return double - the content weight attribute.  
+//         */
+//         virtual double getWeight() const = 0;
+
+//         /*!
+//             Sets the value attribute in the Cattle Class.
+//             \param cattle_value which will be set to the current Cattle.
+//         */
+//         virtual void setValue(double cattle_value) = 0;
+
+//         /*!
+//             Returns the value attribute in the Cattle Class.
+//             \return double - the content value attribute.  
+//         */
+//         virtual double getValue() const = 0;
+
+// };
+
+// #endif
