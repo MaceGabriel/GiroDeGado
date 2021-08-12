099bccf798919c292b6118166d380db49a0f2d0d
gabriel mace <gabrielmace1@hotmail.com>
2021-08-12 17:12:15 -0300

    Tentativas de leitura e escrita no banco

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@ int main(int argc, char *argv[])
 {   
     QSqlDatabase bancoDeDados = QSqlDatabase::addDatabase("QSQLITE");
     //Mudar diretório pelo o de sua máquina. Não esqueça de inverter as barras.
-    bancoDeDados.setDatabaseName("C:/Users/marcu/Downloads/UFOP/Engenharia/Projeto/GiroDeGado/bd_giro.db");
+    QString path = "C:/Users/Gabriel Mace/Desktop/Faculdade/20.2/Engenharia de Software/GiroDeGado/bd_giro.db";
+    bancoDeDados.setDatabaseName(path);
 
     if(bancoDeDados.open()){
         std::cout << "BD connected" << std::endl;
@@ -18,8 +19,19 @@ int main(int argc, char *argv[])
     }
 
     QSqlQuery query;
-    query.exec("insert into transaction (value, description, date, cattle_earring)"
-               "values (10.0, 'teste', '01/11/2020', '123456')");
+    //query.exec("insert into transaction (value, description, date, cattle_earring)"
+    //           "values (10.0, 'teste', '01/11/2020', '123456')");
+
+    std::cout<<query.exec("SELECT date FROM transaction WHERE id = 3")<<std::endl;
+    //query.first();
+    //QString b = query.value(0).toString();
+    //std::cout<<b.toStdString()<<std::endl;
+
+    QSqlQuery query1("SELECT date FROM transaction");
+        while (query1.next()) {
+            QString model = query1.value(0).toString();
+            std::cout<<model.toStdString()<<std::endl;
+        }
 
     //main_unit_test();
 
